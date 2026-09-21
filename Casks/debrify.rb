945e148b10c55e882c50fa7f6054f26e3cc3178a
Tyler O'Farrell <tylerofarrell3650@gmail.com>
2026-09-21 19:58:22 +1000

    just got updated to version 0.9.7-alpha.1

--- Casks/debrify.rb
+++ Casks/debrify.rb
@@ -1,5 +1,5 @@
 cask "debrify" do
-  version "0.9.6-alpha.1"
+  version "0.9.7-alpha.1"
   sha256 "da32cbc28a16a7739109eb5f42bda02dac8bb00c65f7d844489c6139f1e857b5"
 
   url "https://github.com/varunsalian/debrify/releases/download/v#{version}/debrify-v#{version}.dmg"
@@ -7,7 +7,7 @@ cask "debrify" do
   desc "Your personal media hub: One app to browse, stream, and organize media from your own services — with a cinematic player built in"
   homepage "https://debrify.tv/"
 
-  app "debrify.app"
+  app "Debrify.app"
 
   zap trash: [
     "~/Library/Application Scripts/com.example.torrentSearchApp",
